76bba890683963bfd2b55d5b7a0e74582d9659a4
eniwez <enzonicolas.pf@gmail.com>
2025-12-17 14:17:57 -0300

    feat(auth): agregar botón de logout que elimina token del navegador

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,12 +1,14 @@
-import { ChevronLeftIcon } from "lucide-react";
+import { ChevronLeftIcon, LogOut } from "lucide-react";
 import VoteItemCard from "../components/VoteItemCard";
 import { useVoteHistory, type VoteHistoryItem } from "../hooks/useVoteHistory";
 import { useNavigate } from "react-router";
 import Loading from "../components/Loading";
+import { useAuth } from "../context/AuthContext";
 
 export default function HistoryPage() {
   const { data, isLoading, error } = useVoteHistory();
   const navigate = useNavigate();
+  const { logout } = useAuth();
 
   if (isLoading) return <Loading />;
   if (error) return <p>Error</p>;
@@ -17,13 +19,28 @@ export default function HistoryPage() {
     data?.filter((vote: VoteHistoryItem) => vote.type === "dislike").length ??
     0;
 
+  const handleLogOut = () => {
+    logout();
+    navigate("/login");
+  };
+
   return (
     <>
-      <div className="fixed top-0 left-0 w-full flex items-center bg-white p-4 shadow-lg ">
-        <button onClick={() => navigate(-1)} className="p-2 mr-4 rounded-full ">
-          <ChevronLeftIcon className="w-6 h-6 text-gray-700" />
+      <div className="fixed top-0 left-0 w-full flex items-center justify-between bg-white p-4 shadow-lg">
+        <div className="flex items-center">
+          <button
+            onClick={() => navigate(-1)}
+            className="p-2 rounded-full mr-2"
+          >
+            <ChevronLeftIcon className="w-6 h-6 text-gray-700" />
+          </button>
+
+          <h2 className="text-xl font-bold">Historial de votos</h2>
+        </div>
+
+        <button onClick={handleLogOut} className="p-2 rounded-full">
+          <LogOut className="w-6 h-6 text-red-500 cursor-pointer hover:text-red-700" />
         </button>
-        <h2 className="text-xl font-bold">Historial de votos</h2>
       </div>
       <div className="max-w-md mx-auto mt-30 px-2">
         <p className="text-sm text-black font-bold mb-4 flex items-center gap-4">
